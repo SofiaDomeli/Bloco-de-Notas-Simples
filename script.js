bf45bd4f716eb6734e43fff5775d36e20c5fd885
Luis Rodrigues <lclaudio2004@hotmail.com>
2024-01-01 22:24:46 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const listaNotas = document.querySelector('.notas')
+const criarNota = document.querySelector('.criarNotas')
+const notas = document.querySelectorAll('.txtNota')
+
+const hoje = new Date() 
+document.getElementById('data').innerHTML = hoje.toLocaleDateString() + ' - ' + hoje.getHours()+ ':' + hoje.getMinutes().toString().padStart(2,"0")
+
+criarNota.addEventListener("click", () => {
+    let textoNota = document.createElement('p')
+    let img = document.createElement('img')
+    textoNota.className = 'txtNota'
+    textoNota.setAttribute("contenteditable", "true")
+    img.src = "images/lixeira.png"
+    listaNotas.appendChild(textoNota).appendChild(img)
+})
+
+listaNotas.addEventListener("click", function(e){
+    if(e.target.tagName === "IMG") {
+        e.target.parentElement.remove()
+    }
+})
